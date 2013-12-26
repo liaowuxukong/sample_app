29b703a486882d4489c103cfc98da00cbbc5f708
liaowuxukong <liaowuxukong@163.com>
2013-12-26 20:25:05 +0800

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,4 +27,5 @@ class User < ActiveRecord::Base
   has_secure_password
 
 
+
 end
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name    "liaowuxukong"
+    email   "liaowuxukong@163.com"
+    password  "foobar"
+    password_confirmation "foobar"
+  end
+end
